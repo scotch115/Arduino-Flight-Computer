a0ca05c034d62e12167eb728cac5a539d724fdc2
scotch115 <jordangamache115@gmail.com>
2020-09-25 15:29:31 -0400

    Added visibility to serial console

--- acceldemo.ino
+++ acceldemo.ino
@@ -32,28 +32,30 @@ void setup(void) {
 //    Serial.println("Couldnt start");
 //    while (1) yield();
 //  }
-//  Serial.println("LIS3DH found!");
+   sout.write("LIS3DH found!");
 //
-//  // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
+   lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
 //
-//  Serial.print("Range = "); Serial.print(2 << lis.getRange());
-//  Serial.println("G");
+    sout.write("Range = ");
+    sout.write(2 << lis.getRange());
+    sout.write("G");
 //
-//  // lis.setDataRate(LIS3DH_DATARATE_50_HZ);
-//  Serial.print("Data rate set to: ");
-//  switch (lis.getDataRate()) {
-//    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
-//    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
-//    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
-//    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
-//    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
-//    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
-//    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
-//
-//    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
-//    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
-//    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
-//  }
+  lis.setDataRate(LIS3DH_DATARATE_50_HZ);
+  sout.write("Data rate set to: ");
+  switch (lis.getDataRate()) {
+    case LIS3DH_DATARATE_1_HZ: sout.write("1 Hz"); break;
+    case LIS3DH_DATARATE_10_HZ: sout.write("10 Hz"); break;
+    case LIS3DH_DATARATE_25_HZ: sout.write("25 Hz"); break;
+    case LIS3DH_DATARATE_50_HZ: sout.write("50 Hz"); break;
+    case LIS3DH_DATARATE_100_HZ: sout.write("100 Hz"); break;
+    case LIS3DH_DATARATE_200_HZ: sout.write("200 Hz"); break;
+    case LIS3DH_DATARATE_400_HZ: sout.write("400 Hz"); break;
+
+
+   case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
+   case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
+   case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
+  }
 }
 
 void loop() {
